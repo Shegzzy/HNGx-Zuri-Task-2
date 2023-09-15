506a4d164d971bcf8db6562d88603ff8be68a490
Segun <miksegun1996@gmail.com>
2023-09-15 20:10:17 +0100

    completed

--- lib/pages/cv_edit_page.dart
+++ lib/pages/cv_edit_page.dart
@@ -5,8 +5,9 @@ import '../models/cv_model.dart';
 
 class EditCV extends StatefulWidget {
   final CV cv;
+  final Function(CV) onUpdate;
 
-  EditCV(this.cv);
+  EditCV(this.cv, this.onUpdate);
 
   @override
   _EditCVState createState() => _EditCVState();
@@ -33,40 +34,130 @@ class _EditCVState extends State<EditCV> {
       appBar: AppBar(
         title: Text('Edit CV'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextField(
-              controller: fullNameController,
-              decoration: InputDecoration(labelText: 'Full Name'),
-            ),
-            TextField(
-              controller: slackUsernameController,
-              decoration: InputDecoration(labelText: 'Slack Username'),
-            ),
-            TextField(
-              controller: githubHandleController,
-              decoration: InputDecoration(labelText: 'GitHub Handle'),
-            ),
-            TextField(
-              controller: bioController,
-              decoration: InputDecoration(labelText: 'Bio'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  widget.cv.fullName = fullNameController.text;
-                  widget.cv.slackUsername = slackUsernameController.text;
-                  widget.cv.githubHandle = githubHandleController.text;
-                  widget.cv.bio = bioController.text;
-                });
-                Navigator.pop(context);
-              },
-              child: Text('Save'),
+      body: Container(
+        height: double.maxFinite,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.grey[200]?.withOpacity(0.7), // Adjust opacity as needed
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5), // Shadow color and opacity
+              spreadRadius: 2, // Spread radius
+              blurRadius: 5, // Blur radius
+              offset: const Offset(0, 3), // Offset
             ),
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 6,),
+                TextField(
+                  controller: fullNameController,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(8),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(
+                            color: Colors.black54,
+                            width: 1
+                        ),
+                      ),
+                    prefixIcon: Icon(Icons.person),
+                    labelText: 'Full Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide(
+                        color: Colors.lightBlue,
+                        width: 1,
+                      ),
+                    )
+                  ),
+
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: slackUsernameController,
+                  decoration: InputDecoration(labelText: 'Slack Username',
+                      contentPadding: EdgeInsets.all(8),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(
+                            color: Colors.black54,
+                            width: 1
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.lightBlue,
+                      width: 1,
+                    ),
+                  )),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: githubHandleController,
+                  decoration: InputDecoration(labelText: 'GitHub Handle',
+                      contentPadding: EdgeInsets.all(8),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(
+                            color: Colors.black54,
+                            width: 1
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.lightBlue,
+                      width: 1,
+                    ),
+                  )),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: bioController,
+                  maxLines: 20,
+                  decoration: InputDecoration(labelText: 'Bio',
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(
+                          color: Colors.black54,
+                          width: 1
+                        ),
+                      ),
+                      contentPadding: EdgeInsets.all(8),
+                      border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: BorderSide(
+                      color: Colors.lightBlue,
+                      width: 1,
+                    ),
+                  )),
+                ),
+                SizedBox(height: 20,),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final updatedCV = CV(
+                        fullName: fullNameController.text,
+                        slackUsername: slackUsernameController.text,
+                        githubHandle: githubHandleController.text,
+                        bio: bioController.text,
+                      );
+                      widget.onUpdate(updatedCV);
+                      Navigator.pop(context);
+                    },
+                    child: Text('Update'),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/pages/cv_page.dart
+++ lib/pages/cv_page.dart
@@ -11,37 +11,111 @@ class CVPageView extends StatefulWidget {
 
 class _CVPageViewState extends State<CVPageView> {
   CV cv = CV(
-    fullName: 'John Doe',
-    slackUsername: 'john_doe',
-    githubHandle: 'johndoe',
-    bio: 'Passionate Flutter Developer',
+    fullName: 'Alabi Micheal Segun',
+    slackUsername: 'Segun Micheal',
+    githubHandle: 'github.com/shegzzy',
+    bio: 'I am an innovative and motivated Full-stack Developer who seeks growth'
+        ' by consistently learning, unlearning, and relearning. I have over four (4) years'
+        ' of experience in Web and Mobile development and have worked on challenging '
+        'and engaging projects that required me to work outside my knowledge set.'
+        ' Also, convert business/user requirements into maintainable and scalable code.'
+        ' My technical expertise includes fluency in Dart (framework: Flutter), react, HTML/CSS, JavaScript, Python,'
+        ' and Django knowledge in continuous integration and deployment/delivery using GitLab CI, GitHub actions unit testing '
+        'using Flutter, and much more. I have been instrumental in the planning and development process of applications I worked '
+        'on either single-handedly or as part of a team. I pay strong attention to details and commitment'
+        ' to delivering quality results. I am well-talented and well-positioned to contribute to any project I engage'
+        ' with as a proactive team player',
   );
+
+  void updateCV(CV updatedCV) {
+    setState(() {
+      cv = updatedCV;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('My CV'),
+        centerTitle: true,
+        backgroundColor: Colors.grey.shade300,
+        actions: <Widget>[
+          IconButton(onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => EditCV(cv, updateCV)),
+            );
+          },
+              icon: const Icon(Icons.edit_note,
+              size: 26,
+                color: Colors.black54,
+              )
+          )
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Full Name: ${cv.fullName}'),
-            Text('Slack Username: ${cv.slackUsername}'),
-            Text('GitHub Handle: ${cv.githubHandle}'),
-            Text('Bio: ${cv.bio}'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EditCV(cv)),
-                );
-              },
-              child: Text('Edit CV'),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 224, 156, 101),
+                Color.fromARGB(255, 65, 36, 114)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Center(
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/me_pic.png'),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Full Name: ${cv.fullName}',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+
+                  ),
+                ),
+                Divider(height: 1,),
+                const SizedBox(height: 10),
+                Text('Slack Username: ${cv.slackUsername}',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+
+                  ),
+                ),
+                Divider(height: 1,),
+                const SizedBox(height: 10),
+                Text('GitHub Handle: ${cv.githubHandle}',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+
+                  ),
+                ),
+                Divider(height: 1,),
+                const SizedBox(height: 10),
+                Text('Bio: ${cv.bio}',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
